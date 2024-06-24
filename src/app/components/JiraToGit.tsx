2fb7970fc239a5ba766794188a969e9a14dca68f
Alexandre <contact@alexvweb.fr>
2024-06-24 23:35:16 +0200

    Fix field JIRA

--- src/app/components/JiraToGit.tsx
+++ src/app/components/JiraToGit.tsx
@@ -10,6 +10,7 @@ const JiraToGit = () => {
 
     const [commit, setCommit] = useState<string>('');
     const [branch, setBranch] = useState<string>('');
+    const [disabled, setDisabled] = useState<boolean>(false);
 
     const promptAi = {
         model: "gpt-3.5-turbo",
@@ -24,7 +25,7 @@ const JiraToGit = () => {
                         Generate a corresponding commit and branch name according to the information in the following ticket:\n
                         Ticket Title: ${title}\n
                         Expected Commit Format: ${prefix}-${numberTicket}: {create a short English description of the title}\n
-                        Expected Branch Format: ${type}/${prefix}-${numberTicket}_{lowercase description with underscores in English}\n
+                        Expected Branch Format: ${type}/${prefix}-${numberTicket}/{lowercase description with underscores in English}\n
                         Ton retour doit être :\n
                         commit:{commit};branch:{branch}
                     `
@@ -56,6 +57,7 @@ const JiraToGit = () => {
             const branchRegex = /branch:(.*)/i;
             const branchMatch = generatedText.match(branchRegex);
             const branch = branchMatch ? branchMatch[1].trim() : '';
+            setDisabled(false);
 
             return {commit, branch};
         }
@@ -65,10 +67,10 @@ const JiraToGit = () => {
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
+        setDisabled(true)
         const {commit, branch} = await generate();
         setCommit(commit);
         setBranch(branch);
-        console.log({commit, branch});
     }
 
     return <>
@@ -76,12 +78,23 @@ const JiraToGit = () => {
         <form className="flex flex-col gap-4 mt-2">
             {/*  Champs de saisie */}
             <div className="flex gap-4">
+
                 <input
+                    max={999}
+                    maxLength={3}
                     className="min-w-[150px] p-4 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-800 border-0 rounded-lg focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 lg:text-lg"
-                    type="text"
+                    type="number"
                     placeholder="NumberTicket"
-                    onChange={(e) => setNumberTicket(e.target.value)}
+                    onChange={(e) => {
+                        const value = e.target.value;
+                        if (value.length <= 3) {
+                            setNumberTicket(value);
+                        } else {
+                            e.target.value = value.slice(0, 3);
+                        }
+                    }}
                 />
+
                 <input
                     className="min-w-[500px] p-4 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-800 border-0 rounded-lg focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 lg:text-lg"
                     type="text"
@@ -98,35 +111,40 @@ const JiraToGit = () => {
                     <option value="feat">Feature</option>
                 </select>
                 <button
-                    className="p-4 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-800 border-0 rounded-lg focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 lg:text-lg"
+                    className={"p-4 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-800 border-0 rounded-lg focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 lg:text-lg"
+                    + (disabled || !numberTicket || !title ? ' cursor-not-allowed' : ' cursor-pointer')}
                     onClick={handleSubmit}
+                    disabled={disabled || !numberTicket || !title}
                 >
                     Generate
                 </button>
             </div>
 
             {/*  Champs de résultats commit et branches */}
-            <div className="flex gap-4">
-                <div className="w-full flex flex-col gap-4">
-                    <label className="text-gray-900 dark:text-gray-100 lg:text-lg">Commit</label>
-                    <input
-                        className="w-full p-4 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-800 border-0 rounded-lg focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 lg:text-lg"
-                        type="text"
-                        placeholder="Commit"
-                        readOnly
-                        defaultValue={commit}
-                    />
+            {
+                (commit && branch) &&
+                <div className="flex gap-4">
+                    <div className="w-full flex flex-col gap-4">
+                        <label className="text-gray-900 dark:text-gray-100 lg:text-lg">Commit</label>
+                        <input
+                            className="w-full p-4 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-800 border-0 rounded-lg focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 lg:text-lg"
+                            type="text"
+                            placeholder="Commit"
+                            readOnly
+                            defaultValue={commit}
+                        />
 
-                    <label className="text-gray-900 dark:text-gray-100 lg:text-lg">Branch</label>
-                    <input
-                        className="w-full p-4 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-800 border-0 rounded-lg focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 lg:text-lg"
-                        type="text"
-                        placeholder="Branch"
-                        readOnly
-                        defaultValue={branch}
-                    />
+                        <label className="text-gray-900 dark:text-gray-100 lg:text-lg">Branch</label>
+                        <input
+                            className="w-full p-4 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-800 border-0 rounded-lg focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 lg:text-lg"
+                            type="text"
+                            placeholder="Branch"
+                            readOnly
+                            defaultValue={branch}
+                        />
+                    </div>
                 </div>
-            </div>
+            }
         </form>
     </>
         ;
