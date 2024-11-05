689ef8c622d194257f8d14130c3adcfb664e1ffb
AlexVWeb <contact@alexvweb.fr>
2024-11-05 15:16:30 +0100

    fix: build layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,28 +3,21 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "DevTools",
-  description: "A collection of tools for developers.",
+    title: "DevTools",
+    description: "A collection of tools for developers.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fr">
-        <head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>{metadata.title}</title>
-            <meta name="description" content={metadata.description} />
-            <link rel="icon" href="/favicon.ico" />
-        </head>
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="fr">
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
